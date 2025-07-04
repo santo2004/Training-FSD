44dfb83d95ad9b3680a66dbb52e7c0824af635ba
Santo <santo2004off@gmail.com>
2025-07-04 09:56:54 +0530

    tried to ftech data from a fake api

--- Frontend/React/React-App/src/CourseList.jsx
+++ Frontend/React/React-App/src/CourseList.jsx
@@ -7,21 +7,28 @@ function CourseList() {
 
     const [dummy,setDummy] = useState(false);
 
-    useEffect( () => {
-        fetch('https://localhost:3000')
-        .then( response => {
-            //console.log(response);
-            return response.json()
-        }).then (data => setCourses(data))
-    },[]);     //can make dependency array like '[dummy]' '[courses]' or '[]' for no dependency
+    useEffect(() => {
+    fetch('http://localhost:4000/courses')
+        .then(response => response.json())
+        .then(data => setCourses(data))
+        .catch(err => {
+        console.error('Failed to fetch:', err);
+        setCourses([]); // fallback to empty list
+        });
+    }, []);    //can make dependency array like '[dummy]' '[courses]' or '[]' for no dependency
 
     function handleDelete(id) {
         const newCourses = courses.filter((course) => course.id != id);
         setCourses(newCourses);
     }
 
-    //courses.sort((a,b) => b.price - a.price); //descending order by price
+    // courses.sort((a,b) => b.price - a.price); //descending order by price
     // courses.sort((a,b) => a.rating - b.rating); //ascending order by rating
+
+    if (!courses)
+    {
+        return <></>
+    }
 
     const filteredCourses = courses.filter((course) => course.price>10);
 
